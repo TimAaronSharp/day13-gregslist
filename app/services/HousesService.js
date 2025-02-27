6ac0f6d0f67c644a3ede0759867d2045a77717ae
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-02-26 20:15:37 -0700

    Can delete houses from AppState. Data is locally saved/loaded in browser

--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState.js";
 import { House } from "../models/House.js";
+import { loadState, saveState } from "../utils/Store.js";
 
 
 
@@ -14,9 +15,38 @@ class HousesService {
     const houses = AppState.houses
     houses.push(newHouse)
 
+    this.saveHouses()
+
     console.log(houses);
 
+  }
 
+  deleteHouse(houseID) {
+    const houses = AppState.houses
+    const houseIndex = houses.findIndex(house => house.id == houseID)
+
+    console.log(`Index is ${houseIndex}`);
+
+    houses.splice(houseIndex, 1)
+
+    this.saveHouses()
+    console.log(houses);
+
+  }
+
+  saveHouses() {
+    saveState('houses', AppState.houses)
+  }
+
+  loadHouses() {
+    const housesFromLocalStorage = loadState('houses', [House])
+
+    if (housesFromLocalStorage.length == 0) {
+      AppState.emit('houses')
+      return
+    }
+
+    AppState.houses = housesFromLocalStorage
   }
 }
 
